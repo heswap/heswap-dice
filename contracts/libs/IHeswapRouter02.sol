29f80bf0fc17ed830b65ed19e61f32d0781b9aec
bitfriend <bitfriend@hotmail.com>
2021-07-16 12:26:16 +0200

    Fix compiling errors

--- contracts/libs/IHeswapRouter02.sol
+++ contracts/libs/IHeswapRouter02.sol
@@ -1,4 +1,6 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2;
+
 interface IHeswapRouter02 {
     function factory() external pure returns (address);
     function WETH() external pure returns (address);
